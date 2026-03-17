abeb351a1e3a91c1419c81996370e154c2012e08
Ashish-Mekanism <ashish.dev@tech.mekanism.cc>
2026-03-17 15:53:02 +0530

    remove build folder for render

--- build/src/models/Action.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const constants_1 = require("../utils/constants");
-const ActionSchema = new mongoose_1.Schema({
-    Action: { type: String, required: true, unique: true },
-});
-const Action = (0, mongoose_1.model)(constants_1.DbModel.Action, ActionSchema);
-exports.default = Action;
--- build/src/models/Counter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const CounterSchema = new mongoose_1.Schema({
-    _id: { type: String, required: true },
-    seq: { type: Number, default: 0 },
-});
-const Counter = (0, mongoose_1.model)('Counter', CounterSchema);
-exports.default = Counter;
